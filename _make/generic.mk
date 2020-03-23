67d916c87e4a8d22efd07d17c2064ac5f6eb38ed
Kavit Shah <kashah@adobe.com>
2020-03-23 11:41:32 -0700

    saving work

--- _make/generic.mk
+++ _make/generic.mk
@@ -1,3 +1,7 @@
+# https://www.tutorialspoint.com/makefile/makefile_rules.htm
+# https://stackoverflow.com/questions/2270643/what-is-a-make-target
+# https://stackoverflow.com/questions/3220277/what-do-the-makefile-symbols-and-mean
+
 # compiler/linker configs
 CXX = clang++
 LD = clang++
@@ -6,5 +10,11 @@ CFLAGS = -Wall
 # output releated configs
 EXE = main
 
-all:
-	${CXX} ${CFLAGS} ${FILES} -o ${EXE} 
+# first default rule
+#${} also works 
+all: $(EXE)
+
+$(EXE): $(OBJS)	
+	$(CXX) $(CFLAGS) $(FILES) -o $(EXE) 
+# Todo: replaced files with objects $(CXX) $(CFLAGS) $(FILES) -o $(EXE) 
+# Todo: put object files into seperate directory
