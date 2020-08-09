02b155bf2af5ad4d9f2c7c16ab215f6d24964de7
Kavit Shah <kashah@adobe.com>
2020-08-08 22:29:42 -0700

    string & tuples

---
+++ basics/strings/main.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// #include <string>
+#include <cstdio>
+
+int main() {
+    // std::string str1("white civic");
+    // std::string str2("green civic");
+    // if (str1.compare(str2)) {
+
+    // }
+    puts("Hello world \n");
+    // c strings or primitive string
+    char str[] = "string";
+    printf("string is %s", str);
+    // char str1[] = {"s", "t", "r", "i", "n", "g"}; should work but it does not
+    // printf("string is %s\n", str1);
+    return 0;
+}
