40a89c7d18c4b2ed0cb1b976380a5ade18019b69
Kavit Shah <kashah@adobe.com>
2020-08-08 22:28:00 -0700

    cout

--- basics/std/cout/main.cpp
+++ basics/std/cout/main.cpp
@@ -9,9 +9,7 @@ int main() {
     char b = 'B';
     // main.cpp:7:10: error: cannot initialize a variable of type 'char' with an lvalue of type 'const char [2]'
     // char b = "B";
-
     // No need to define type %d %c like printf, puts
-    cout << a << " Hello World! " << b << endl;    
-
+    cout << a << " Hello World! " << b << endl;
     return 0;
 }
