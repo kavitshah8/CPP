074faa2831a5ac9628ec0e57294d731822528350
Kavit Shah <kashah@adobe.com>
2020-08-17 17:14:29 -0700

    add comments for struct

--- basics/struct/main.cpp
+++ basics/struct/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-struct AnalyticsName {
+struct AnalyticsName
+{
     std::string googleAnalytics;
     std::string amazonAnalytics;
 };
 
-int main() {
+int main()
+{
+    // Note the it assigns values based in order
     AnalyticsName Application = {"g_application", "a_application"};
     std::cout << Application.googleAnalytics << std::endl;
     std::cout << Application.amazonAnalytics << std::endl;
--- basics/tuples/main.cpp
+++ basics/tuples/main.cpp
@@ -4,11 +4,9 @@ using namespace std;
 
 // To comipe: g++ main.cpp -std=c++11
 int main() {
-
-    tuple <int, int, int> node;
-    node = make_tuple(1, 2, 3);
-    std::cout << get<0>(node) << std::endl;
-    std::cout << get<1>(node) << std::endl;
-    std::cout << get<2>(node) << std::endl;
+    tuple <int, int, int> node = make_tuple(1, 2, 3);
+    cout << get<0>(node) << endl;
+    cout << get<1>(node) << endl;
+    cout << get<2>(node) << endl;
     return 0;
 }
