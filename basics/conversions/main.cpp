468ce769e2aee190188db14c564c3cf1c989da95
Kavit Shah <kashah@adobe.com>
2020-09-03 07:26:44 -0700

    update user defined cast example

--- basics/conversions/main.cpp
+++ basics/conversions/main.cpp
@@ -4,6 +4,10 @@
 // https://en.cppreference.com/w/cpp/language/cast_operator
 // https://stackoverflow.com/questions/15999123/const-before-parameter-vs-const-after-function-name-c
 
+// Note the difference between user defined casting and language casting
+// https://en.cppreference.com/w/cpp/language/implicit_conversion
+// https://en.cppreference.com/w/c/language/conversion
+
 // Interfaces
 struct T {
     std::string a;
@@ -11,6 +15,7 @@ struct T {
     std::string c;
 
     operator std::string() const {
+        std::cout << "User defined cast got invokesd" << std::endl;
         return this->a;
     }
 };
@@ -20,11 +25,11 @@ void log(T, T, T);
 
 // Implementations
 void log(std::string a, std::string b, std::string c) {
-    std::cout << "A = " << a << ", B = " << b << ", C = " << c << std::endl;
+    std::cout << "A = " << a << ", B = " << b << ", C = " << c << "\n" << std::endl;
 }
 
 void log(T a, T b, T c) {
-    std::cout << "T A = " << a.a << ", T B = " << b.b << ", T C = " << c.c << std::endl;
+    std::cout << "T A = " << a.a << ", T B = " << b.b << ", T C = " << c.c << "\n" << std::endl;
 }
 
 int main() {
@@ -33,6 +38,10 @@ int main() {
     T c = {"TFoo", "TBro", "TBro"};
     log("foo", "bar", "bro");
     log(a, b, c);
+
+    // Note we do not have an inteface which accepts the following calls
+    // And our user defined casts kicks in 
     log("Foo", "Bar", c);
+    log("Foo", b, c);
     return 0;
 }
